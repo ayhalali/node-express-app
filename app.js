fb0adb002e0b0a3a2d1d72a2553a25f034ae29d7
ayhalali <ayhalali@hotmail.com>
2019-10-22 12:22:39 -0500

    Update files

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ const port = process.env.PORT || config.get("port");
 app.get('/', function (req, res) {
   res.send('Welcome to the default page!  <br> <br>' +
     'Try going to different URIs by adding these at the end: <br> <br>' +
-    '/hello <br>' +
+    '/hi <br>' +
     '/big <br>' +
     '/json <br>' +
-    '/greeting/yourname <br>' +
-    '/yo/Dr.Rogers <br>' +
+    '/greeting/Ali <br>' +
+    '/hello/Ali <br>' +
     '/fortune <br>' +
-    '/fancy/?first=Denise&last=Case <br>' +
+    '/fancy/?first=Ali&last=AlAli <br>' +
     '<br> <br>' +
     'Fork the source code from <a href="https://github.com/denisecase/node-express-app">https://github.com/denisecase/node-express-app</a>'
   )
@@ -34,18 +34,18 @@ app.get('/', function (req, res) {
 
 // or use the new arrow function syntax
 // respond with text
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+app.get('/hi', (req, res) => {
+  res.send('Welcome to Express framwork!')
 })
 
 // or respond with html
 app.get('/big', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1><strong>Hello There!</strong></h1>')
 })
 
 // or respond with JSON
 app.get('/json', (req, res) => {
-  res.send('{"name" : "Nandini"}')
+  res.send('{<br>name : "Ali", <br>class: "Web Apps"<br>}')
 })
 
 // :name indicates a parameter at this location in the URI
@@ -54,8 +54,8 @@ app.get('/greeting/:id', (req, res) => {
 })
 
 // combine your skills and get creative
-app.get('/yo/:buddy', (req, res) => {
-  res.send(`<h1>Yo, ${req.params.buddy}!</h1>`)
+app.get('/hello/:buddy', (req, res) => {
+  res.send(`<h1>Welcome, ${req.params.buddy}!</h1>`)
 })
 
 // provide multiple query parameters (named first and last) with ? and &
@@ -66,39 +66,38 @@ app.get('/fancy', (req, res) => {
 })
 
 let fortunes = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.',
-'You may rely on it', 'As I see it, yes.', 'Most likely', 'Outlook good.', 'Yes.', 'Signs point to yes.',
+'You may rely on it', 'As I see it, yes.', 'Most likely', 'Yes.', 'Signs point to yes.',
 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 
-'Concentrate and ask again.', 'Don\'t count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.',
-'Very doubtful.']
+'Concentrate and ask again.', 'My sources say yes.', 'Very doubtful.']
 
 // Implements a Magic 8 Ball service
 app.get('/fortune', (req,res) => {
   if(isEmpty(req.query)){
     res.send('<h2>You wish to know the future?</h2>' +
-             '<p>Ask a question in the query string, e.g., http://localhost:3002/fortune?Will I become rich? <br/>' +
+             '<p>Ask a question in the query string, e.g., http://localhost:3002/fortune? Will I get an A in this class? <br/>' +
              '<p>The Magic 8 Ball will answer!</p>')
   } else {
-    res.send(`The answer is ... wait for it ... ${fortunes[randomInt(0, fortunes.length)]}`)
+    res.send(`<h2>The answer is ... wait for it ...</h2><br>...<br>...<h3>${fortunes[randomInt(0, fortunes.length)]}</h3>`)
   }
 })
 
 // Use middleware to handle all non-managed routes (e.g. /xyz)
 // https://expressjs.com/en/api.html#req.originalUrl
 app.use((req, res, next) => {
-  res.status(404).send(`status 404 - ${req.originalUrl} was not found`);
+  res.status(404).send(`Error 404 - page (${req.originalUrl})  was not found`);
 })
 
 // start listening and inform developers
 app.listen(port, hostname, () => {
   console.log(`\n App listening at http://${hostname}:${port}/`)
   console.log(`\n Try going to different URIs:\n`)
-  console.log(`   Try /hello`)
+  console.log(`   Try /hi`)
   console.log(`   Try /big`)
   console.log(`   Try /json`)
   console.log(`   Try /fortune`)
   console.log(`   Try /greeting/yourname`)
-  console.log(`   Try /yo/Dr.Rogers`)
-  console.log(`   Try /fancy/?first=Denise&last=Case`)
+  console.log(`   Try /hello/Ali`)
+  console.log(`   Try /fancy/?first=Ali&last=AlAli`)
   console.log('\n Hit CTRL-C CTRL-C to stop\n')
 })
 
